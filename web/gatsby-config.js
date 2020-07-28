f0341a56bdea2ac5b44c007f3978be8ee545100b
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-07-28 09:20:15 -0300

    added GA plugin settings to gatsby-config.js

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,32 +1,45 @@
 // Load variables from `.env` as soon as possible
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV || 'development'}`
-})
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
 
-const clientConfig = require('./client-config')
+const clientConfig = require("./client-config");
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   plugins: [
-    'gatsby-plugin-postcss',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-plugin-google-tagmanager',
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        id: 'GTM-5C64MPR',
-        includeInDevelopment: false,
-        defaultDataLayer: { platform: 'gatsby' }
-      }
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-113321962-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+      },
     },
     {
-      resolve: 'gatsby-source-sanity',
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-5C64MPR",
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    },
+    {
+      resolve: "gatsby-source-sanity",
       options: {
         ...clientConfig.sanity,
         token: process.env.SANITY_READ_TOKEN,
         watchMode: !isProd,
-        overlayDrafts: !isProd
-      }
+        overlayDrafts: !isProd,
+      },
     },
     `gatsby-plugin-sharp`,
     {
@@ -35,28 +48,26 @@ module.exports = {
         name: `passiv-theme`,
         short_name: `passiv`,
         start_url: `/`,
-        icon: `src/images/logo-no-name.png`
-      }
+        icon: `src/images/logo-no-name.png`,
+      },
     },
-    'gatsby-plugin-sitemap',
+    "gatsby-plugin-sitemap",
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-robots-txt",
       options: {
-        host: 'https://getpassiv.com',
-        sitemap: 'https://getpassiv.com/sitemap.xml',
-        policy: [{
-          userAgent: '*',
-          allow: '/',
-          disallow: [
-            '/admin',
-            '/app',
-            '/api',
-          ],
-        }],
-      }
+        host: "https://getpassiv.com",
+        sitemap: "https://getpassiv.com/sitemap.xml",
+        policy: [
+          {
+            userAgent: "*",
+            allow: "/",
+            disallow: ["/admin", "/app", "/api"],
+          },
+        ],
+      },
     },
   ],
   siteMetadata: {
     siteUrl: `https://getpassiv.com`,
   },
-}
+};
