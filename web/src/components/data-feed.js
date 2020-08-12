0ee8dde85e628cdd93347daad577cf37c033d368
Kirsten <kirsten.jvd@gmail.com>
2020-08-12 15:32:20 -0700

    Include font awesome, state, success constant, axios, and added the progress bar and moved the title, posttype and date to the top

--- web/src/components/data-feed.js
+++ web/src/components/data-feed.js
@@ -1,15 +1,24 @@
 import {format, distanceInWords, differenceInDays} from 'date-fns'
-import React from 'react'
+import React, { useState }  from 'react'
 import {buildImageObj} from '../lib/helpers'
 import {imageUrlFor} from '../lib/image-url'
 import PortableText from './portableText'
 import Container from './container'
 const ReactMarkdown = require('react-markdown')
+import axios from 'axios';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {faFacebook,faTwitter,faLinkedinIn} from "@fortawesome/free-brands-svg-icons"
+
+import ReadingProgress from 'react-reading-progress'
 
 import styles from './blog-post.module.css'
+import {Link} from 'gatsby'
+
 
 function DataFeed (props) {
   const {body, title, publishedAt, postType} = props
+  const [success, setSuccess] = useState(false);
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -28,73 +37,95 @@ function DataFeed (props) {
   }
   
   return (
-    <article className={styles.root}>
-      <Container>
-        <div className={styles.grid}>
-        <div className={styles.wrapper}>
-          <div className={styles.sidebar}>
-            <div className={styles.sidebarContainer}>
-              <div className={styles.share}>
-                <h2>Share</h2>
-                <a
-                  target="_blank"
-                  href={`https://twitter.com/intent/tweet/?text=${
-                    title
-                  }&url=https://getpassiv.com/blog/${props.slug.current}%2F&via=getpassiv`}>
-                  <FontAwesomeIcon icon={faTwitter} className={styles.icon}/>
-                </a>
-                <a
-                  href={`https://www.facebook.com/sharer/sharer.php?u=https://getpassiv.com/blog/${
-                    props.slug.current
-                  }`}
-                  target="_blank">
-                  <FontAwesomeIcon icon={faFacebook} className={styles.icon}/>
-                </a>
-                <a
-                  href={`https://www.linkedin.com/shareArticle?mini=true&url=https://getpassiv.com/blog/${
-                    props.slug.current
-                  }&title=${title}&source=${title}`}
-                  target="_blank">
-                  <FontAwesomeIcon icon={faLinkedinIn} className={styles.icon}/>
-                </a>
+    <div className={styles.feed}>
+      <ReadingProgress targetEl="#targetEl" />
+
+      <article id="targetEl" className={styles.root}>
+        <Container>
+          <div className={styles.metaContainer}>
+            <div className={styles.metaDetails}>
+              {postType && (
+                <Link className={styles.category} to={postType} >
+                  {postType}
+                </Link>
+              )}
+              <div>
+                <h1 className={styles.blogTitle}>{title}</h1>
               </div>
-              <div className={styles.formContainer}>
-                <h2>Stay up to date</h2>
-
-                <form onSubmit={handleSubmit}>
-                  <label for="email">
-                    Email
-                    <input id="email" name="email" required="" type="email"/>
-                  </label>
-                  { success ? (
-                    <button type="submit" disabled>Success</button>
-                  ) : (
-                    <button type="submit">Get Updates</button>
-                  )}
-
-                </form>
-
+              <div className={styles.whoWhen}>
+                {publishedAt && (
+                  <span className={styles.publishedAt}>
+                    {differenceInDays(new Date(publishedAt), new Date()) > 3
+                      ? distanceInWords(new Date(publishedAt), new Date())
+                      : format(new Date(publishedAt), 'MMMM Do, YYYY')}
+                  </span>
+                )}
               </div>
             </div>
           </div>
-          </div>
-          <div className={styles.mainContent}>
-            <h1>{title}</h1>
-            <ReactMarkdown
-              source={body}
-              escapeHtml={false}
-            />
-            {publishedAt && (
-              <div className={styles.publishedAt}>
-                {differenceInDays(new Date(publishedAt), new Date()) > 3
-                  ? distanceInWords(new Date(publishedAt), new Date())
-                  : format(new Date(publishedAt), 'MMMM Do, YYYY')}
+          <div className={styles.wrapper}>
+            <div className={styles.sidebar}>
+              <div className={styles.sidebarContainer}>
+                <div className={styles.share}>
+                  <h2>Share</h2>
+                  <a
+                    target="_blank"
+                    href={`https://twitter.com/intent/tweet/?text=${
+                      title
+                    }&url=https://getpassiv.com/blog/${props.slug.current}%2F&via=getpassiv`}>
+                    <FontAwesomeIcon icon={faTwitter} className={styles.icon}/>
+                  </a>
+                  <a
+                    href={`https://www.facebook.com/sharer/sharer.php?u=https://getpassiv.com/blog/${
+                      props.slug.current
+                    }`}
+                    target="_blank">
+                    <FontAwesomeIcon icon={faFacebook} className={styles.icon}/>
+                  </a>
+                  <a
+                    href={`https://www.linkedin.com/shareArticle?mini=true&url=https://getpassiv.com/blog/${
+                      props.slug.current
+                    }&title=${title}&source=${title}`}
+                    target="_blank">
+                    <FontAwesomeIcon icon={faLinkedinIn} className={styles.icon}/>
+                  </a>
+                </div>
+                <div className={styles.formContainer}>
+                  <h2>Stay up to date</h2>
+
+                  <form onSubmit={handleSubmit}>
+                    <label for="email">
+                      Email
+                      <input id="email" name="email" required="" type="email"/>
+                    </label>
+                    { success ? (
+                      <button type="submit" disabled>Success</button>
+                    ) : (
+                      <button type="submit">Get Updates</button>
+                    )}
+
+                  </form>
+
+                </div>
               </div>
-            )}
+            </div>
+            <div className={styles.mainContent}>
+              <ReactMarkdown
+                source={body}
+                escapeHtml={false}
+              />
+              {publishedAt && (
+                <div className={styles.publishedAt}>
+                  {differenceInDays(new Date(publishedAt), new Date()) > 3
+                    ? distanceInWords(new Date(publishedAt), new Date())
+                    : format(new Date(publishedAt), 'MMMM Do, YYYY')}
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      </Container>
-    </article>
+        </Container>
+      </article>
+    </div>
   )
 }
 
