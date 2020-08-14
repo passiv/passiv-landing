830c96622514cc3106f67be878973cd406ddc92f
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-08-14 17:07:15 -0300

    created schema for model portfolios

---
+++ studio/schemas/documents/modelPortfolio.js
@@ -0,0 +1,96 @@
+import { format } from 'date-fns'
+
+export default {
+  name: 'modelPortfolio',
+  type: 'document',
+  title: 'Model Portfolios',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+      description: 'Titles should be catchy, descriptive, and not too long'
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      description: 'Some frontends will require a slug to be set to be able to show the post',
+      options: {
+        source: 'title',
+        maxLength: 96,
+        slugify: input =>
+          input
+            .toLowerCase()
+            .replace(/\s+/g, '-')
+            .slice(0, 200)
+      }
+    },
+    {
+      name: 'publishedAt',
+      type: 'datetime',
+      title: 'Published at',
+      description: 'This can be used to schedule post for publishing'
+    },
+    {
+      title: 'Contributor',
+      name: 'contributor',
+      type: 'string',
+      description: 'The name of the person (real or pseudonymous) who contributed this model portfolio'
+      }
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'markdown',
+      options: {
+        minRows: 20
+      }
+    }
+  ],
+  orderings: [
+    {
+      name: 'publishingDateAsc',
+      title: 'Publishing date new–>old',
+      by: [
+        {
+          field: 'publishedAt',
+          direction: 'asc'
+        },
+        {
+          field: 'title',
+          direction: 'asc'
+        }
+      ]
+    },
+    {
+      name: 'publishingDateDesc',
+      title: 'Publishing date old->new',
+      by: [
+        {
+          field: 'publishedAt',
+          direction: 'desc'
+        },
+        {
+          field: 'title',
+          direction: 'asc'
+        }
+      ]
+    }
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      publishedAt: 'publishedAt',
+      slug: 'slug'
+    },
+    prepare({ title = 'No title', publishedAt, slug = {}, media }) {
+      const dateSegment = format(publishedAt, 'YYYY/MM')
+      const path = `/${dateSegment}/${slug.current}/`
+      return {
+        title,
+        subtitle: publishedAt ? path : 'Missing publishing date'
+      }
+    }
+  }
+}
