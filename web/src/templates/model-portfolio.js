8e7b47c7b73f2314ecc2749167a44031fc87e57f
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-08-14 17:15:17 -0300

    set up model portfolio template

---
+++ web/src/templates/model-portfolio.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Container from "../components/container";
+import GraphQLErrorList from "../components/graphql-error-list";
+import DataFeed from "../components/data-feed";
+import SEO from "../components/seo";
+import Layout from "../containers/layout";
+import { toPlainText } from "../lib/helpers";
+
+export const query = graphql`
+  query modelPortfolioTemplateQuery($id: String!) {
+    feed: sanityModelPortfolio(id: { eq: $id }) {
+      id
+      publishedAt
+      title
+      slug {
+        current
+      }
+      postType
+      body
+    }
+  }
+`;
+
+const modelPortfolioTemplate = (props) => {
+  const { data, errors } = props;
+  const feed = data && data.feed;
+  return (
+    <Layout>
+      {errors && <SEO title="GraphQL Error" />}
+      {feed && <SEO title={feed.title || "Untitled"} />}
+
+      {errors && (
+        <Container>
+          <GraphQLErrorList errors={errors} />
+        </Container>
+      )}
+      {feed && <DataFeed {...feed} />}
+    </Layout>
+  );
+};
+
+export default modelPortfolioTemplate;
