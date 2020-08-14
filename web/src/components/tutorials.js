761a62de1cafec472f6c1638be187d3516212529
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-08-14 10:55:51 -0300

    fixed <div> issue

--- web/src/components/tutorials.js
+++ web/src/components/tutorials.js
@@ -32,49 +32,48 @@ function Tutorials(props) {
   return (
     <article className={styles.root}>
       <Container>
-      <div className={styles.wrapper}>
-        <div className={styles.sidebar}></div>
-        <div className={styles.sidebarContainer}>
-          <div className={styles.share}>
-            <h2>Share</h2>
-            <a
-              target="_blank"
-              href={`https://twitter.com/intent/tweet/?text=${title}&url=https://getpassiv.com/blog/${props.slug.current}%2F&via=getpassiv`}
-            >
-              <FontAwesomeIcon icon={faTwitter} className={styles.icon} />
-            </a>
-            <a
-              href={`https://www.facebook.com/sharer/sharer.php?u=https://getpassiv.com/blog/${props.slug.current}`}
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
-            </a>
-            <a
-              href={`https://www.linkedin.com/shareArticle?mini=true&url=https://getpassiv.com/blog/${props.slug.current}&title=${title}&source=${title}`}
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faLinkedinIn} className={styles.icon} />
-            </a>
-          </div>
-          <div className={styles.formContainer}>
-            <h2>Stay up to date</h2>
+        <div className={styles.wrapper}>
+          <div className={styles.sidebar}></div>
+          <div className={styles.sidebarContainer}>
+            <div className={styles.share}>
+              <h2>Share</h2>
+              <a
+                target="_blank"
+                href={`https://twitter.com/intent/tweet/?text=${title}&url=https://getpassiv.com/blog/${props.slug.current}%2F&via=getpassiv`}
+              >
+                <FontAwesomeIcon icon={faTwitter} className={styles.icon} />
+              </a>
+              <a
+                href={`https://www.facebook.com/sharer/sharer.php?u=https://getpassiv.com/blog/${props.slug.current}`}
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
+              </a>
+              <a
+                href={`https://www.linkedin.com/shareArticle?mini=true&url=https://getpassiv.com/blog/${props.slug.current}&title=${title}&source=${title}`}
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faLinkedinIn} className={styles.icon} />
+              </a>
+            </div>
+            <div className={styles.formContainer}>
+              <h2>Stay up to date</h2>
 
-            <form onSubmit={handleSubmit}>
-              <label for="email">
-                Email
-                <input id="email" name="email" required="" type="email" />
-              </label>
-              {success ? (
-                <button type="submit" disabled>
-                  Success
-                </button>
-              ) : (
-                <button type="submit">Get Updates</button>
-              )}
-            </form>
+              <form onSubmit={handleSubmit}>
+                <label for="email">
+                  Email
+                  <input id="email" name="email" required="" type="email" />
+                </label>
+                {success ? (
+                  <button type="submit" disabled>
+                    Success
+                  </button>
+                ) : (
+                  <button type="submit">Get Updates</button>
+                )}
+              </form>
+            </div>
           </div>
-        </div>
-        </div>
         </div>
         <div className={styles.tutorialPost}>
           <div className={styles.mainContent}>
