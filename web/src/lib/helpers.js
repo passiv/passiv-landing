ce51cdd42c11a150db7a49b4f85569f6376ee66a
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-08-17 13:11:26 -0300

    reverted styling changes in helpers.js

--- web/src/lib/helpers.js
+++ web/src/lib/helpers.js
@@ -1,65 +1,66 @@
-import { format, isFuture } from "date-fns";
-import qs from "qs";
+import {format, isFuture} from 'date-fns'
+import qs from 'qs';
 
-export function cn(...args) {
-  return args.filter(Boolean).join(" ");
+
+export function cn (...args) {
+  return args.filter(Boolean).join(' ')
 }
 
-export function mapEdgesToNodes(data) {
-  if (!data.edges) return [];
-  return data.edges.map((edge) => edge.node);
+export function mapEdgesToNodes (data) {
+  if (!data.edges) return []
+  return data.edges.map(edge => edge.node)
 }
 
-export function filterOutDocsWithoutSlugs({ slug }) {
-  return (slug || {}).current;
+export function filterOutDocsWithoutSlugs ({slug}) {
+  return (slug || {}).current
 }
 
-export function filterOutDocsPublishedInTheFuture({ publishedAt }) {
-  return !isFuture(publishedAt);
+export function filterOutDocsPublishedInTheFuture ({publishedAt}) {
+  return !isFuture(publishedAt)
 }
 
-export function getBlogUrl(publishedAt, slug) {
-  return `/blog/${format(publishedAt, "YYYY/MM")}/${slug.current || slug}/`;
+export function getBlogUrl (publishedAt, slug) {
+  return `/blog/${format(publishedAt, 'YYYY/MM')}/${slug.current || slug}/`
 }
 
-export function buildImageObj(source = { asset: {} }) {
+export function buildImageObj (source = {asset: {}}) {
   const imageObj = {
-    asset: { _ref: source.asset._ref || source.asset._id },
-  };
+    asset: {_ref: source.asset._ref || source.asset._id}
+  }
 
-  if (source.crop) imageObj.crop = source.crop;
-  if (source.hotspot) imageObj.hotspot = source.hotspot;
+  if (source.crop) imageObj.crop = source.crop
+  if (source.hotspot) imageObj.hotspot = source.hotspot
 
-  return imageObj;
+  return imageObj
 }
 
-export function toPlainText(blocks) {
+export function toPlainText (blocks) {
   if (!blocks) {
-    return "";
+    return ''
   }
   return blocks
-    .map((block) => {
-      if (block._type !== "block" || !block.children) {
-        return "";
+    .map(block => {
+      if (block._type !== 'block' || !block.children) {
+        return ''
       }
-      return block.children.map((child) => child.text).join("");
+      return block.children.map(child => child.text).join('')
     })
-    .join("\n\n");
+    .join('\n\n')
 }
 
-export function saveReferralCode() {
-  if (typeof window !== "undefined") {
+export function saveReferralCode () {
+  if (typeof window !== 'undefined') {
     const query_params = qs.parse(window.location.search, {
       ignoreQueryPrefix: true,
     });
     if (query_params.ref !== undefined) {
-      localStorage.setItem("landing-ref", query_params.ref);
+      localStorage.setItem('landing-ref', query_params.ref);
     }
   }
 }
 
-export function getReferralCode() {
-  if (typeof localStorage !== "undefined") {
-    return localStorage.getItem("landing-ref");
+export function getReferralCode () {
+  if (typeof localStorage !== 'undefined') {
+    return localStorage.getItem('landing-ref');
   }
 }
