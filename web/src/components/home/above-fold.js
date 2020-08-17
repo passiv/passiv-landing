5eea1370dd4e9a896c0e9e406082bad35ba3b148
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-08-17 11:55:29 -0300

    added getSignups() function

--- web/src/components/home/above-fold.js
+++ web/src/components/home/above-fold.js
@@ -1,38 +1,53 @@
-import React from 'react'
-import {buildImageObj,cn} from '../../lib/helpers'
-import {imageUrlFor} from '../../lib/image-url'
-import Container from '../container'
-import { getReferralCode } from '../../lib/helpers';
+import React from "react";
+import { buildImageObj, cn, getSignups } from "../../lib/helpers";
+import { imageUrlFor } from "../../lib/image-url";
+import Container from "../container";
+import { getReferralCode } from "../../lib/helpers";
+import axios from "axios";
+import styles from "./above-fold.module.css";
 
-import styles from './above-fold.module.css'
-
-function AboveFold ({  }) {
+function AboveFold({}) {
   const referralCode = getReferralCode();
+  console.log(typeof getSignups());
   return (
     <section className={styles.aboveFold}>
       <Container>
         <div className={styles.ctaContainer}>
           <h1>Autopilot for your Portfolio</h1>
-          <p className={styles.blurb}>Passiv turns your brokerage account into a modern portfolio management tool. Build your own personalized index, invest and rebalance with the click of a button, and seamlessly manage multiple accounts.</p>
+          <p className={styles.blurb}>
+            Passiv turns your brokerage account into a modern portfolio management tool. Build your
+            own personalized index, invest and rebalance with the click of a button, and seamlessly
+            manage multiple accounts.
+          </p>
           <div className={cn(styles.emailSignup, styles.inputContainer)}>
             <div className={cn(styles.emailContainer, styles.formContainer)}>
               <form className={styles.register} method="get" noValidate action="/app/register/">
-                <label><span>Enter your Email</span></label>
+                <label>
+                  <span>Enter your Email</span>
+                </label>
                 <input type="hidden" name="type" />
-                <input type="email" required max_length="512" className={styles.registerEmail} name="email" />
-                <input className={cn(styles.btn1, styles.registerBtn, styles.clickping)} type="submit" value="Get Passiv" />
-                {
-                  referralCode !== null && (
-                    <input type="hidden" name="ref" value={referralCode} />
-                  )
-                }
+                <input
+                  type="email"
+                  required
+                  max_length="512"
+                  className={styles.registerEmail}
+                  name="email"
+                />
+                <br></br>
+                <br></br>
+                <input
+                  className={cn(styles.btn1, styles.registerBtn, styles.clickping)}
+                  type="submit"
+                  value={`Join 442 New Users This Month`}
+                />
+                {referralCode !== null && <input type="hidden" name="ref" value={referralCode} />}
               </form>
             </div>
           </div>
         </div>
       </Container>
     </section>
-  )
+  );
 }
 
-export default AboveFold
+export default AboveFold;
