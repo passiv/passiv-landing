39d312281bb9eb86a486309263cfd8f40624a2aa
Nick McCullum <nicholasmccullum@gmail.com>
2020-08-25 16:26:58 -0300

    created new components for MoneyGeek index

---
+++ web/src/components/moneygeek-post-preview-grid.js
@@ -0,0 +1,34 @@
+import {Link} from 'gatsby'
+import React from 'react'
+import MoneyGeekPostPreview from './moneygeek-post-preview'
+
+import styles from './blog-post-preview-grid.module.css'
+
+function MoneyGeekPostPreviewGrid (props) {
+  return (
+    <div className={styles.root}>
+      {props.title && <h2 className={styles.headline}>{props.title}</h2>}
+      <ul className={styles.grid}>
+        {props.nodes &&
+          props.nodes.map(node => (
+            <li key={node.id}>
+              <MoneyGeekPostPreview {...node} isInGrid />
+            </li>
+          ))}
+      </ul>
+      {props.browseMoreHref && (
+        <div className={styles.browseMoreNav}>
+          <Link to={props.browseMoreHref + '/'}>Browse More</Link>
+        </div>
+      )}
+    </div>
+  )
+}
+
+MoneyGeekPostPreviewGrid.defaultProps = {
+  title: '',
+  nodes: [],
+  browseMoreHref: ''
+}
+
+export default MoneyGeekPostPreviewGrid
--- web/src/pages/moneygeek.js
+++ web/src/pages/moneygeek.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {graphql} from 'gatsby'
 import {mapEdgesToNodes} from '../lib/helpers'
-import BlogPostPreviewGrid from '../components/blog-post-preview-grid'
+import MoneyGeekPostPreviewGrid from '../components/moneygeek-post-preview-grid'
 import Container from '../components/container'
 import GraphQLErrorList from '../components/graphql-error-list'
 import SEO from '../components/seo'
@@ -52,7 +52,7 @@ const BlogMoneyGeekPage = props => {
       <SEO title='Our Blog | Money Geek' />
       <Container>
         <h1 className={responsiveTitle1}>Money Geek</h1>
-        {postNodes && postNodes.length > 0 && <BlogPostPreviewGrid nodes={postNodes} />}
+        {postNodes && postNodes.length > 0 && <MoneyGeekPostPreviewGrid nodes={postNodes} />}
       </Container>
     </Layout>
   )
