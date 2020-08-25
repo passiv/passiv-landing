d496f3fd57602d44b74a36e8902165d58ecd63b0
Nick McCullum <nicholasmccullum@gmail.com>
2020-08-25 16:28:54 -0300

    removed images on moneygeek index

--- web/src/components/moneygeek-post-preview.js
+++ web/src/components/moneygeek-post-preview.js
@@ -14,18 +14,6 @@ function MoneyGeekPostPreview (props) {
       className={props.isInList ? styles.inList : styles.inGrid}
       to={`/weblog/${props.slug.current}/`}
     >
-      <div className={styles.leadMediaThumb}>
-        {props.mainImage && props.mainImage.asset && (
-          <img
-            src={imageUrlFor(buildImageObj(props.mainImage))
-              .width(600)
-              .height(Math.floor((9 / 16) * 600))
-              .auto('format')
-              .url()}
-            alt={props.mainImage.alt}
-          />
-        )}
-      </div>
       <div className={styles.text}>
         <h3 className={cn(responsiveTitle3, styles.title)}>{props.title}</h3>
         <div className={styles.date}>{format(props.publishedAt, 'MMMM Do, YYYY')}</div>
