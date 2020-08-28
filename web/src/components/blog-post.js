ed0f5f3afe574134cd75db8bc7e50cd3e4d80718
Nick McCullum <nicholasmccullum@gmail.com>
2020-08-28 13:33:50 -0300

    fixed broken link on every blog page

--- web/src/components/blog-post.js
+++ web/src/components/blog-post.js
@@ -46,7 +46,7 @@ function BlogPost (props) {
         <div className={styles.metaContainer}>
           <div className={styles.metaDetails}>
             {postType && (
-              <Link className={styles.category} to="https://passiv.com/blog/" >
+              <Link className={styles.category} to="/blog/" >
 
               {/* <Link className={styles.category} to="blog" > */}
                 {postType}
