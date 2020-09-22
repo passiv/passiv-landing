8e500186a024fb6de35e109dfb48d5788af9f9a1
Nick McCullum <nicholasmccullum@gmail.com>
2020-09-22 12:24:10 -0300

    created howToBuy schema

---
+++ studio/schemas/documents/howToBuy.js
@@ -0,0 +1,45 @@
+import { format } from 'date-fns'
+
+export default {
+    name: 'post',
+    type: 'document',
+    title: 'Blog Post',
+    fields: [
+        {
+            name: 'ticker',
+            type: 'string',
+            title: 'Ticker',
+        },
+        {
+            name: 'companyName',
+            type: 'string',
+            title: 'Company Name',
+        },
+        {
+            name: 'slug',
+            type: 'slug',
+            title: 'Slug',
+            description: 'Some frontends will require a slug to be set to be able to show the post',
+            options: {
+                source: 'title',
+                maxLength: 96
+            }
+        },
+        {
+            name: 'publishedAt',
+            type: 'datetime',
+            title: 'Published at',
+            description: 'This can be used to schedule post for publishing'
+        },
+        {
+            name: 'authors',
+            title: 'Authors',
+            type: 'array',
+            of: [
+                {
+                    type: 'authorReference'
+                }
+            ]
+        },
+    ],
+}
