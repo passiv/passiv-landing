75eac21c75381271af928f5910e8115da630e035
Nick McCullum <nicholasmccullum@gmail.com>
2020-09-22 13:04:01 -0300

    added the "How to Invest" articles to Sanity Studio

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -4,65 +4,69 @@ import MdPerson from 'react-icons/lib/md/person'
 import MdLabel from 'react-icons/lib/md/label'
 
 const hiddenDocTypes = listItem =>
-  !['category', 'author', 'post', 'tutorial', 'siteSettings', 'dataFeed', 'modelPortfolio', 'postMoneyGeek'].includes(listItem.getId())
+    !['category', 'author', 'post', 'tutorial', 'siteSettings', 'dataFeed', 'modelPortfolio', 'postMoneyGeek'].includes(listItem.getId())
 
 export default () =>
-  S.list()
-    .title('Content')
-    .items([
-      S.listItem()
-        .title('Settings')
-        .icon(MdSettings)
-        .child(
-          S.editor()
-            .id('siteSettings')
-            .schemaType('siteSettings')
-            .documentId('siteSettings')
-        ),
-      S.listItem()
-        .title('Blogs')
-        .child(
-          S.list()
-            .title('Blogs')
-            .items([
-              S.listItem()
-                .title('The Index')
+    S.list()
+        .title('Content')
+        .items([
+            S.listItem()
+                .title('Settings')
+                .icon(MdSettings)
+                .child(
+                    S.editor()
+                        .id('siteSettings')
+                        .schemaType('siteSettings')
+                        .documentId('siteSettings')
+                ),
+            S.listItem()
+                .title('Blogs')
+                .child(
+                    S.list()
+                        .title('Blogs')
+                        .items([
+                            S.listItem()
+                                .title('The Index')
+                                .schemaType('post')
+                                .child(S.documentTypeList('post').title('The Index Posts')),
+                            S.listItem()
+                                .title('MoneyGeek')
+                                .schemaType('post')
+                                .child(S.documentTypeList('postMoneyGeek').title('Money Geek Posts')),
+                            S.listItem()
+                                .title('Authors')
+                                .icon(MdPerson)
+                                .schemaType('author')
+                                .child(S.documentTypeList('author').title('Authors')),
+                            S.listItem()
+                                .title('Categories')
+                                .icon(MdLabel)
+                                .schemaType('category')
+                                .child(S.documentTypeList('category').title('Categories')),
+                        ])
+                ),
+            S.listItem()
+                .title('Tutorials')
                 .schemaType('post')
-                .child(S.documentTypeList('post').title('The Index Posts')),
-              S.listItem()
-                .title('MoneyGeek')
+                .child(S.documentTypeList('tutorial').title('Tutorials')),
+            S.listItem()
+                .title('Data feeds')
                 .schemaType('post')
-                .child(S.documentTypeList('postMoneyGeek').title('Money Geek Posts')),
-              S.listItem()
-                .title('Authors')
-                .icon(MdPerson)
-                .schemaType('author')
-                .child(S.documentTypeList('author').title('Authors')),
-              S.listItem()
-                .title('Categories')
-                .icon(MdLabel)
-                .schemaType('category')
-                .child(S.documentTypeList('category').title('Categories')),
-              ])  
-          ),
-      S.listItem()
-        .title('Tutorials')
-        .schemaType('post')
-        .child(S.documentTypeList('tutorial').title('Tutorials')),
-      S.listItem()
-        .title('Data feeds')
-        .schemaType('post')
-        .child(S.documentTypeList('dataFeed').title('Data Feed')),
-      S.listItem()
-        .title('Model portfolios')
-        .schemaType('post')
-        .child(S.documentTypeList('modelPortfolio').title('Model portfolios')),
-      S.listItem()
-        .title('Landing Pages')
-        .schemaType('post')
-        .child(S.documentTypeList('landing').title('Landing Pages')),
-      
-      // This returns an array of all the document types
-      // defined in schema.js. We filter out those that we have
-      // defined the structure above
-    ])
+                .child(S.documentTypeList('dataFeed').title('Data Feed')),
+            S.listItem()
+                .title('Model portfolios')
+                .schemaType('post')
+                .child(S.documentTypeList('modelPortfolio').title('Model portfolios')),
+            S.listItem()
+                .title('Landing Pages')
+                .schemaType('post')
+                .child(S.documentTypeList('landing').title('Landing Pages')),
+            S.listItem()
+                .title('How to Invest $X')
+                .schemaType('howToInvest')
+                .child(S.documentTypeList('howToInvest').title('How to Invest $X')),
+
+            // This returns an array of all the document types
+            // defined in schema.js. We filter out those that we have
+            // defined the structure above
+        ])
