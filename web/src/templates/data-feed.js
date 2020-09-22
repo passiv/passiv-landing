93f516de08ca61960da3b36a31ff95b7f957b1b3
Nick McCullum <nicholasmccullum@gmail.com>
2020-09-22 12:09:45 -0300

    more data feed improvements

--- web/src/templates/data-feed.js
+++ web/src/templates/data-feed.js
@@ -1,25 +1,42 @@
+/* eslint-disable indent */
 import React from 'react'
+import {graphql} from 'gatsby'
 import Container from '../components/container'
 import GraphQLErrorList from '../components/graphql-error-list'
 import DataFeed from '../components/data-feed'
 import SEO from '../components/seo'
 import Layout from '../containers/layout'
 
+export const query = graphql`
+  query dataFeedTemplateQuery($id: String!) {
+    feed: sanityDataFeed(id: {eq: $id}) {
+      id
+      publishedAt
+      title
+      slug {
+        current
+      }
+      postType
+      body
+    }
+  }
+`
+
 const dataFeedTemplate = props => {
-    const { data, errors } = props
+    const {data, errors} = props
     const feed = data && data.feed
     return (
-        <Layout>
-            {errors && <SEO title='GraphQL Error' />}
-            {feed && <SEO title={feed.title || 'Untitled'} />}
+      <Layout>
+        {errors && <SEO title='GraphQL Error' />}
+        {feed && <SEO title={feed.title || 'Untitled'} />}
 
-            {errors && (
-                <Container>
-                    <GraphQLErrorList errors={errors} />
-                </Container>
+        {errors && (
+        <Container>
+          <GraphQLErrorList errors={errors} />
+        </Container>
             )}
-            {feed && <DataFeed {...feed} />}
-        </Layout>
+        {feed && <DataFeed {...feed} />}
+      </Layout>
     )
 }
 
